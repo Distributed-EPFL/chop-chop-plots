500cee9ef0876207149161938b9605ef5adc077e
Pierre-Louis Roman <plroman@acm.org>
2024-05-09 21:33:20 +0200

    extract_chopchop*py: merge linerate script into main extract chopchop script

--- extract_chopchop_comma.py
+++ extract_chopchop_comma.py
@@ -7,7 +7,9 @@ import json
 ### local import
 import utils
 
+
 def output_throughput(heartbeat_path):
+    """ Find throughput value in a file """
     stdout = subprocess.run([utils.DIR_CHOPCHOP + "/target/release/heartbeat_statistics", "--shallow-server", heartbeat_path, "--start", "30", "--duration", "60"], stdout = subprocess.PIPE, stderr = subprocess.DEVNULL).stdout
     stdout = str(stdout)
     stdout = stdout.split("\n")
@@ -18,12 +20,41 @@ def output_throughput(heartbeat_path):
     
     raise "Malformed heartbeat output!"
 
+
+def total_messages(heartbeat_path):
+    """ Find number of messages delivered in a file """
+    stdout = subprocess.run([utils.DIR_CHOPCHOP + "/target/release/heartbeat_statistics", "--shallow-server", heartbeat_path], stdout = subprocess.PIPE, stderr = subprocess.DEVNULL).stdout
+    stdout = str(stdout)
+    stdout = stdout.split("\n")
+
+    for line in stdout:
+        if "Deliveries:" in line:
+            return int(line.split(":")[1].split("(")[0])
+
+    raise "Malformed heartbeat output!"
+
+
 def latencies(log_path):
+    """ Find all latencies in a file """
     lines = open(log_path).readlines()
     deliveries = [line for line in lines if "Message delivered!" in line]
     latencies = [int(line.split("Took")[1].split("ms")[0]) for line in deliveries]
     
     return latencies
+
+
+def network_transfer(before_path, after_path):
+    before_lines = open(before_path).readlines()
+    after_lines = open(after_path).readlines()
+
+    befores = [line for line in before_lines if "ens5:rx_bytes:" in line]
+    afters = [line for line in after_lines if "ens5:rx_bytes:" in line]
+
+    before = int(befores[0].split(":")[2])
+    after = int(afters[0].split(":")[2])
+
+    return after - before
+
 
 def heartbeat_paths(broadcast, load_broker_throughput, base_path, matching_trusted):
     # Careful: include in the filter the batch size of the TOB.
@@ -56,6 +87,7 @@ def heartbeat_paths(broadcast, load_broker_throughput, base_path, matching_trust
     
     return heartbeat_paths
 
+
 def client_log_paths(broadcast, load_broker_throughput, base_path):
     load_broker_throughput_filter = "6-18-16_" + str(load_broker_throughput)
 
@@ -83,6 +115,44 @@ def client_log_paths(broadcast, load_broker_throughput, base_path):
     
     return heartbeat_paths
 
+
+def linerate_paths(broadcast, load_broker_throughput, base_path):
+    load_broker_throughput_filter = "64-64-6-18-16_" + str(load_broker_throughput)
+    secondary_filter = "8_400_2022"
+
+    linerate_paths = []
+
+    vaults = os.listdir(base_path)
+    vaults = [vault for vault in vaults if broadcast in vault]
+    vaults = [vault for vault in vaults if load_broker_throughput_filter in vault and secondary_filter in vault]
+
+    for vault in vaults:
+        vault_path = base_path + "/" + vault
+
+        processes = os.listdir(vault_path)
+        servers = [process for process in processes if "result_server_" in process]
+
+        for server in servers:
+            server_path = vault_path + "/" + server
+
+            files = os.listdir(server_path)
+
+            heartbeats = [file for file in files if ".bin" in file]
+            befores = [file for file in files if ".before" in file]
+            afters = [file for file in files if ".after" in file]
+
+            if len(heartbeats) > 1:
+                print("WHAT THE HELL??")
+            elif len(heartbeats) > 0:
+                heartbeat = server_path + "/" + heartbeats[0]
+                before = server_path + "/" + befores[0]
+                after = server_path + "/" + afters[0]
+
+                linerate_paths.append({"heartbeat": heartbeat, "before": before, "after": after})
+
+    return linerate_paths
+
+
 ######## Throughputs ########
 def compute_throughput(base_path, input_rates, destination, payload_size, matching_trusted):
     plot = {}
@@ -109,6 +179,7 @@ def compute_throughput(base_path, input_rates, destination, payload_size, matchi
     print("Created json file: " + filename)
     # print("\n\n\n")
     # print(contents)
+
 
 ####### Latencies ########
 def compute_latency(base_path, input_rates, destination, payload_size):
@@ -140,11 +211,43 @@ def compute_latency(base_path, input_rates, destination, payload_size):
     # print(contents)
 
 
+######## Linerate ########
+def compute_linerate(base_path, input_rates, destination, payload_size):
+    plot = {}
+
+    for broadcast in ["bftsmart", "hotstuff"]:
+        plot[broadcast] = {}
+
+        for load_broker_throughput in input_rates:
+            input_throughput = load_broker_throughput + 432000  / payload_size * 8.
+            input_throughput = float(input_throughput) / 1000000.
+
+            plot[broadcast][input_throughput] = []
+
+            for linerate_path in linerate_paths(broadcast, load_broker_throughput, base_path):
+                output_throughput_value = output_throughput(linerate_path["heartbeat"])
+                output_total_messages = total_messages(linerate_path["heartbeat"])
+                network_transfer_value = network_transfer(linerate_path["before"], linerate_path["after"])
+
+                goodput = float(output_total_messages) * 11.5 / float(network_transfer_value)
+
+                plot[broadcast][input_throughput].append({"output_throughput": output_throughput_value, "goodput": goodput})
+
+                # print(broadcast + " @ " + str(input_throughput) + " -> " + str(output_throughput_value) + "  (" + str(goodput) + ")")
+
+    contents = json.dumps(plot)
+    filename = 'linerate_' + destination + '.json'
+    with open(filename, 'a') as f:
+        f.write(contents)
+    print("Created json file: " + filename)
+    # print("\n\n\n")
+    # print(contents)
+
 
 
 
 #####
-##### Main
+##### Throughput latency measures
 #####
 
 ARGS = [
@@ -165,3 +268,14 @@ ARGS = [
 for arg in ARGS:
     compute_latency(arg[0], arg[1], arg[2], arg[3])
     compute_throughput(arg[0], arg[1], arg[2], arg[3], arg[4])
+
+#####
+##### Linerate
+#####
+
+ARGS = [
+    (utils.DIR_RESULT + "/linerate", [10000000, 20000000, 30000000, 40000000, 50000000, 60000000], "chopchop", 8)
+    ]
+
+for arg in ARGS:
+    compute_linerate(args[0], args[1], args[2], args[3])
